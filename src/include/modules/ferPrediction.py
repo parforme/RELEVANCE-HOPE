86938d7fa1716ad477955baea9292b01919eb730
Benjamin Rainer <benjamin.rainer@ait.ac.at>
2021-08-16 16:35:34 +0200

    defined FER prediction result message

--- src/include/modules/ferPrediction.py
+++ src/include/modules/ferPrediction.py
@@ -1,6 +1,7 @@
 from hsys import module
 from hsys import databroker
 from msg import ferPredictionMsg
+from msg import ferPredictionMsgResult
 import rospy
 
 class ferPrediction(module.Module):
@@ -8,12 +9,12 @@ class ferPrediction(module.Module):
         super(ferPrediction, self).__init__()
      
     def callback(self, data):
+        # TODO: implement producer consumer pattern, such that we can handle a lot of prediction requests at once!
         print("Got data: " + data.data)
+        self.pub.publish(1.0)
 
     def initModule(self):
         print("Initializing simplified FER prediction module.")
-        # initialize a publisher
-        self.pub = databroker.addListener("ferPrediction", ferPredictionMsg, self.callback)
-        # run the thread of this module
-        self.start()
-
+        databroker.addListener("ferPrediction", ferPredictionMsg, self.callback)
+        self.pub = databroker.addPublisher("predictedFer", ferPredictionMsgResult, 10)
+        
